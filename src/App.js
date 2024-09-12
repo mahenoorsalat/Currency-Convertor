bf45753f2fb0a23a437d9bc30f521111f63d3857
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2024-09-12 13:37:53 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import gif from './exchange.gif'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css'
+ 
+const App = () => {
+  const [exchangeRates, setExchangeRates] = useState({});
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState('USD');
+  const [toCurrency, setToCurrency] = useState('INR');
+  const [convertedAmount, setConvertedAmount] = useState(null);
+ 
+  useEffect(() => {
+    
+    const apiUrl = `https://api.exchangerate-api.com/v4/latest/${fromCurrency}`;
+     
+    axios.get(apiUrl)
+      .then(response => {
+        setExchangeRates(response.data.rates);
+      })
+      .catch(error => {
+        console.error('Error fetching exchange rates:', error);
+      });
+  }, [fromCurrency]);
+ 
+  useEffect(() => {
+    
+    const conversionRate = exchangeRates[toCurrency];
+    if (conversionRate) {
+      const converted = amount * conversionRate;
+      setConvertedAmount(converted.toFixed(2));
+    }
+  }, [amount, fromCurrency, toCurrency, exchangeRates]);
+ 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'amount':
+        setAmount(value);
+        break;
+      case 'fromCurrency':
+        setFromCurrency(value);
+        break;
+      case 'toCurrency':
+        setToCurrency(value);
+        break;
+      default:
+        break;
+    }
+  };
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='card' >
+      <img src={gif} width={60} alt="" />
+      <h1 className='text-6xl'>Currency Converter</h1>
+ 
+      <div className='currency_exchnage'>
+ 
+      <div className="input_container" >
+        <label className="input_label">Amount:</label>
+        <input
+          type="number"
+          name="amount"
+          className="input_field"
+          value={amount}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="input_container">
+        <label className="input_label">From Currency:</label>
+        <select
+          name="fromCurrency"
+          value={fromCurrency}
+          onChange={handleChange}
+          className="input_field"
         >
-          Learn React
-        </a>
-      </header>
+          {Object.keys(exchangeRates).map(currency => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="input_container">
+        <label className="input_label">To Currency:</label>
+        <select
+          name="toCurrency"
+          value={toCurrency}
+          onChange={handleChange}
+          className="input_field"
+        >
+          {Object.keys(exchangeRates).map(currency => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
+ 
+      </div>
+ 
+      <div className='output'>
+        <h2>Converted Amount: {convertedAmount}</h2>
+      </div>
     </div>
   );
-}
-
+};
+ 
 export default App;
